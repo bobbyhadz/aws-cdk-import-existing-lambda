00522fd7d23b842413773d0197d08bd48bf3fc43
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-02 16:05:41 +0300

    added infrastructure code

---
+++ README.md
@@ -0,0 +1,33 @@
+# Import an Existing Lambda Function in AWS CDK
+
+A repository for an article on
+[bobbyhadz.com](https://bobbyhadz.com/blog/aws-cdk-import-existing-lambda)
+
+## How to Use
+
+1. Clone the repository
+
+2. Install the dependencies
+
+```bash
+npm install
+```
+
+3. Replace the `YOUR_FUNCTION_NAME` placeholder, in the call to
+   `fromFunctionArn` method, with the name of the lambda function you want to
+   import
+
+4. Create the CDK stack
+
+```bash
+npx cdk deploy
+```
+
+4. Open the AWS CloudFormation Console and the stack should be created in your
+   default region
+
+5. Cleanup
+
+```bash
+npx cdk destroy
+```
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,33 @@
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as cdk from '@aws-cdk/core';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 👇 import existing Lambda by ARN
+    const importedLambdaFromArn = lambda.Function.fromFunctionArn(
+      this,
+      'external-lambda-from-arn',
+      `arn:aws:lambda:${cdk.Stack.of(this).region}:${
+        cdk.Stack.of(this).account
+      }:function:YOUR_FUNCTION_NAME`,
+    );
+
+    console.log('functionArn 👉', importedLambdaFromArn.functionArn);
+    console.log('functionName 👉', importedLambdaFromArn.functionName);
+
+    // 👇 create API
+    const api = new apigateway.RestApi(this, 'api');
+
+    // 👇 add a /test route on the API
+    const test = api.root.addResource('test');
+
+    // 👇 integrate imported Lambda at GET /test on the API
+    test.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(importedLambdaFromArn),
+    );
+  }
+}
